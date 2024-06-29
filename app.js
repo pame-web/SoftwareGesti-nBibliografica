80d7651deb9b4d6daf6e19b0bc0f6dfa47c18f2f
pame-web <84988217+pame-web@users.noreply.github.com>
2024-06-28 23:37:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-// Función para cargar y mostrar los datos según la hoja seleccionada
 function cargarDatosTabla(id) {
-    var url = 'https://script.google.com/macros/s/AKfycbxSqi5cRv_08OABOdvsc3QGrm6xvmV4yF6fw2P2Fs51ax31839NQ2V80E5l7ggXz_FlyA/exec?sheet=' + id;
+    var url = 'https://script.google.com/macros/s/AKfycbxJXhKQjwVXLOIskiVsCB3qXRVEUGpiwJgfPbz-8Lqc5uQumoY1OXz9fLcTrU9-94RdwA/exec?sheet=' + id;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            var tbody = document.getElementById('tabla' + id.charAt(0).toUpperCase() + id.slice(1) + 'Body'); // Ejemplo: 'tablaFacultadBody'
+            console.log('Datos recibidos:', data); // Verifica qué datos estás recibiendo
+
+            var tbody = document.getElementById('tabla' + id.charAt(0).toUpperCase() + id.slice(1) + 'Body');
             tbody.innerHTML = ''; // Limpiar tabla antes de llenar
 
             data.forEach(item => {
                 var row = document.createElement('tr');
                 if (id === 'facultad') {
+                    console.log('Item de facultad:', item);
                     row.innerHTML = `
                         <td>${item.id}</td>
                         <td>${item.codigo_facultad}</td>
                         <td>${item.descripcion_facultad}</td>
                     `;
                 } else if (id === 'materia') {
+                    console.log('Item de materia:', item);
                     row.innerHTML = `
                         <td>${item.id}</td>
                         <td>${item.cod_materia}</td>
@@ -24,6 +27,7 @@ function cargarDatosTabla(id) {
                         <td>${item.credito}</td>
                     `;
                 } else if (id === 'material') {
+                    console.log('Item de material:', item);
                     row.innerHTML = `
                         <td>${item.id}</td>
                         <td>${item.edición}</td>
